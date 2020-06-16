599a3fc5c8130a373861f62000c3edfb85368834
Julia Stodolya <y.stodolya@gmail.com>
2020-06-16 20:11:24 +0300

    Initial Commit

---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Юлия on 16.06.2020.
+//  Copyright © 2020 Julia Stodolya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
